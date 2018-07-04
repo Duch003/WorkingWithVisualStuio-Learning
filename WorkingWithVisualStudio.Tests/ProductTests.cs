729f933c3cbe33b7c0c5f3174311ec9364101b75
Duch003 <tomaszmankin003@gmail.com>
2018-07-04 20:27:22 +0100

    Zakończony rozdział

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+            p.Name = "Nowa nazwa";
+
+            Assert.Equal("Nowa nazwa", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+            p.Price = 200M;
+
+            Assert.Equal(200M, p.Price);
+        }
+
+    }
+}
